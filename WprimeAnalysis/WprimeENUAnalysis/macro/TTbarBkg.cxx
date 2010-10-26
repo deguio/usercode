fe27e5454ac4266447e7583c428c69cb609552af
malberti <>
2010-10-26 13:02:39 +0000

    updated with CMS preliminary, etc

--- WprimeAnalysis/WprimeENUAnalysis/macro/TTbarBkg.cxx
+++ WprimeAnalysis/WprimeENUAnalysis/macro/TTbarBkg.cxx
@@ -1,19 +1,22 @@
 
 
 {
-  gROOT->SetStyle("Plain");
+  gROOT->LoadMacro("setTDRStyle.C");
+  setTDRStyle();
+  //gROOT->SetStyle("Plain");
   gStyle->SetOptStat(0);
+  gStyle->SetOptTitle(0);
 
-  float lumi = 4.375;
-    
-  int N = 9;
+  float lumi = 15.0416; //all
+      
+  int N = 10;
   
   int nRe  = 10;
-  int nReB = 2;
+  int nReB = 1;
   
 
   // DATA histos
-  TFile *fData =  TFile::Open("HistosForTTbarBkg_HighPurBdisc/outputHistos_Data_7TeV.root");
+  TFile *fData =  TFile::Open("../bin/HistosTTbar_Btag3.0_Eta2.4_Pt20/outputHistos_Data_7TeV.root");
   TH1F * hmtData    = (TH1F*)fData->Get("hmt");
   TH1F * hmt1bData  = (TH1F*)fData->Get("hmt1b");
   TH1F * hmt2bData  = (TH1F*)fData->Get("hmt2b");
@@ -32,16 +35,17 @@
 
   // MC histos
   TFile *f[10];
-  f[0] = TFile::Open("HistosForTTbarBkg_HighPurBdisc/outputHistos_WprimeM800_7TeV.root");
-  f[1] = TFile::Open("HistosForTTbarBkg_HighPurBdisc/outputHistos_WprimeM1000_7TeV.root");
-  f[2] = TFile::Open("HistosForTTbarBkg_HighPurBdisc/outputHistos_WprimeM1500_7TeV.root");
-  f[3] = TFile::Open("HistosForTTbarBkg_HighPurBdisc/outputHistos_QCD_7TeV.root");
-  f[4] = TFile::Open("HistosForTTbarBkg_HighPurBdisc/outputHistos_Zee_7TeV.root");
-  f[5] = TFile::Open("HistosForTTbarBkg_HighPurBdisc/outputHistos_Dibosons_7TeV.root");
-  f[6] = TFile::Open("HistosForTTbarBkg_HighPurBdisc/outputHistos_Wtaunu_7TeV.root");
-  f[7] = TFile::Open("HistosForTTbarBkg_HighPurBdisc/outputHistos_TTbar_7TeV.root");
-  f[8] = TFile::Open("HistosForTTbarBkg_HighPurBdisc/outputHistos_Wenu_7TeV.root");
-  
+  f[0] = TFile::Open("../bin/HistosTTbar_Btag3.0_Eta2.4_Pt20/outputHistos_WprimeM800_7TeV.root");
+  f[1] = TFile::Open("../bin/HistosTTbar_Btag3.0_Eta2.4_Pt20/outputHistos_WprimeM1000_7TeV.root");
+  f[2] = TFile::Open("../bin/HistosTTbar_Btag3.0_Eta2.4_Pt20/outputHistos_WprimeM1500_7TeV.root");
+  f[3] = TFile::Open("../bin/HistosTTbar_Btag3.0_Eta2.4_Pt20/outputHistos_QCD_7TeV.root");
+  f[4] = TFile::Open("../bin/HistosTTbar_Btag3.0_Eta2.4_Pt20/outputHistos_DY_7TeV.root");
+  f[5] = TFile::Open("../bin/HistosTTbar_Btag3.0_Eta2.4_Pt20/outputHistos_Dibosons_7TeV.root");
+  f[6] = TFile::Open("../bin/HistosTTbar_Btag3.0_Eta2.4_Pt20/outputHistos_Wtaunu_7TeV.root");
+  f[7] = TFile::Open("../bin/HistosTTbar_Btag3.0_Eta2.4_Pt20/outputHistos_TTbar_7TeV.root");
+  f[8] = TFile::Open("../bin/HistosTTbar_Btag3.0_Eta2.4_Pt20/outputHistos_Wenu_7TeV.root");
+  f[9] = TFile::Open("../bin/HistosTTbar_Btag3.0_Eta2.4_Pt20/outputHistos_GammaPlusJets_7TeV.root");
+
   int mycolor;
 
   TH1F *hmt[10];
@@ -68,15 +72,16 @@
     hmt2b[i]->Rebin(nRe);
     hBdisc[i]->Rebin(nReB);
 
-    if (i==0) mycolor = 2; //W'
-    if (i==1) mycolor = 3; //W'
-    if (i==2) mycolor = 4; //W'
-    if (i==3) mycolor = 12; // QCD
-    if (i==4) mycolor = 5; // Zee
-    if (i==5) mycolor = 8; // dibosons 
-    if (i==6) mycolor = 7; // Wtaunu
-    if (i==7) mycolor = 6; // ttbar
-    if (i==8) mycolor = 9; // Wenu
+    if (i==0) mycolor = kRed; //W'
+    if (i==1) mycolor = kRed; //W'
+    if (i==2) mycolor = kRed; //W'
+    if (i==3) mycolor = kGray+2; // QCD
+    if (i==4) mycolor = kOrange+1; // Zee
+    if (i==5) mycolor = kGreen-3; // dibosons 
+    if (i==6) mycolor = kYellow; // Wtaunu
+    if (i==7) mycolor = kMagenta; // ttbar
+    if (i==8) mycolor = kAzure+1; // Wenu
+    if (i==9) mycolor = kPink-2; // Wenu
     
     hmt[i]   -> SetFillColor(mycolor);
     hmt1b[i] -> SetFillColor(mycolor);
@@ -87,21 +92,24 @@
   }
 
  
-  TLegend *leg = new TLegend (0.4,0.6,0.89,0.89);
+  TLegend *leg = new TLegend (0.6,0.6,0.89,0.89);
   leg->SetFillColor(0);
   leg->SetBorderSize(0);
   //leg->AddEntry(hmt[0],"W', M =  800 GeV","F");
   //leg->AddEntry(hmt[1],"W', M = 1000 GeV","F");
   //leg->AddEntry(hmt[2],"W', M = 1500 GeV","F");
-  leg->AddEntry(hmt[3],"QCD ","F");
-  leg->AddEntry(hmt[4],"Z #rightarrow ee","F");
+  leg->AddEntry(hmt[8],"W #rightarrow e #nu","F");
+  leg->AddEntry(hmt[4],"DY #rightarrow ll","F");
   leg->AddEntry(hmt[5],"Dibosons","F");
   leg->AddEntry(hmt[6],"W #rightarrow #tau #nu","F");
   leg->AddEntry(hmt[7],"t#bar{t}","F");
-  leg->AddEntry(hmt[8],"W #rightarrow e #nu","F");
+  leg->AddEntry(hmt[3],"QCD ","F");
+  leg->AddEntry(hmt[9],"#gamma+jets","F");
+  leg->AddEntry(hmtData,"data","PL");
 
   THStack *hNjetsAll = new THStack("hNjetsAll","hNjetsAll");
   hNjetsAll->Add(hNjets[3]);
+  hNjetsAll->Add(hNjets[9]);
   hNjetsAll->Add(hNjets[5]);
   hNjetsAll->Add(hNjets[4]);
   hNjetsAll->Add(hNjets[6]);
@@ -110,6 +118,7 @@
 
   THStack *hBdiscAll = new THStack("hBdiscAll","hBdiscAll");
   hBdiscAll->Add(hBdisc[3]);
+  hBdiscAll->Add(hBdisc[9]);
   hBdiscAll->Add(hBdisc[5]);
   hBdiscAll->Add(hBdisc[4]);
   hBdiscAll->Add(hBdisc[6]);
@@ -118,6 +127,7 @@
 
   THStack *hmt1bAll = new THStack("hmt1bAll","hmt1bAll");
   hmt1bAll->Add(hmt1b[3]);
+  hmt1bAll->Add(hmt1b[9]);
   hmt1bAll->Add(hmt1b[5]);
   hmt1bAll->Add(hmt1b[4]);
   hmt1bAll->Add(hmt1b[6]);
@@ -126,6 +136,7 @@
 
   THStack *hmt2bAll = new THStack("hmt2bAll","hmt2bAll");
   hmt2bAll->Add(hmt2b[3]);
+  hmt2bAll->Add(hmt2b[9]);
   hmt2bAll->Add(hmt2b[5]);
   hmt2bAll->Add(hmt2b[4]);
   hmt2bAll->Add(hmt2b[6]);
@@ -136,7 +147,7 @@
   cnjets->SetLogy();
   hNjetsAll->Draw("a histo");
   hNjetsAll->SetMinimum(0.01);
-  hNjetsAll->SetMaximum(100000);
+  hNjetsAll->SetMaximum(1000000);
   hNjetsAll->GetHistogram()->GetXaxis()->SetRangeUser(0.,6);
   hNjetsAll->GetHistogram()->GetXaxis()->SetTitle("number of jets");
   hNjetsAll->GetHistogram()->GetYaxis()->SetTitle("events");
@@ -144,12 +155,19 @@
   hNjetsAll->Draw("histo");
   hNjetsData->Draw("esame");
   leg->Draw("same");
+  TLatex latex;
+  latex.SetTextFont(42);
+  latex.SetTextSize(0.04);
+  latex.DrawLatex(0.5,500000,"CMS 2010 Preliminary");
+  latex.SetTextSize(0.035);
+  latex.DrawLatex(0.5,100000,"#sqrt{s} = 7 TeV");
+  latex.DrawLatex(0.5,50000,"#intLdt = 15.041 pb^{-1}");
 
   TCanvas * cbdisc = new TCanvas("cbdisc","cbdisc",500,500);
   cbdisc->SetLogy();
   hBdiscAll->Draw("a histo");
-  hBdiscAll->SetMinimum(0.001);
-  hBdiscAll->SetMaximum(20);
+  hBdiscAll->SetMinimum(0.01);
+  hBdiscAll->SetMaximum(200);
   hBdiscAll->GetHistogram()->GetXaxis()->SetRangeUser(0.,6);
   hBdiscAll->GetHistogram()->GetXaxis()->SetTitle("B discriminant");
   hBdiscAll->GetHistogram()->GetYaxis()->SetTitle("events");
@@ -157,12 +175,19 @@
   hBdiscAll->Draw("histo");
   hBdiscData->Draw("esame");
   leg->Draw("same");
+  latex.SetTextFont(42);
+  latex.SetTextSize(0.04);
+  latex.DrawLatex(0.5,150,"CMS 2010 Preliminary");
+  latex.SetTextSize(0.035);
+  latex.DrawLatex(0.5,80,"#sqrt{s} = 7 TeV");
+  latex.DrawLatex(0.5,30,"#intLdt = 15.041 pb^{-1}");
+
 
   TCanvas * c1b = new TCanvas("c1b","c1b",500,500);
   c1b->SetLogy();
   hmt1bAll->Draw("a histo");
   hmt1bAll->SetMinimum(0.01);
-  hmt1bAll->SetMaximum(10);
+  hmt1bAll->SetMaximum(100);
   hmt1bAll->GetHistogram()->GetXaxis()->SetRangeUser(0.,500);
   hmt1bAll->GetHistogram()->GetXaxis()->SetTitle("M^{T} (GeV)");
   hmt1bAll->GetHistogram()->GetYaxis()->SetTitle("events/10 GeV");
@@ -170,12 +195,18 @@
   hmt1bAll->Draw("histo");
   hmt1bData->Draw("esame");
   leg->Draw("same");
+  latex.SetTextFont(42);
+  latex.SetTextSize(0.04);
+  latex.DrawLatex(20,90,"CMS 2010 Preliminary");
+  latex.SetTextSize(0.035);
+  latex.DrawLatex(20,42,"#sqrt{s} = 7 TeV");
+  latex.DrawLatex(300,0.5,"#intLdt = 15.041 pb^{-1}");
 
   TCanvas * c2b = new TCanvas("c2b","c2b",500,500);
   c2b->SetLogy();
   hmt2bAll->Draw("a histo");
-  hmt2bAll->SetMinimum(0.0001);
-  hmt2bAll->SetMaximum(1.0);
+  hmt2bAll->SetMinimum(0.01);
+  hmt2bAll->SetMaximum(100.0);
   hmt2bAll->GetHistogram()->GetXaxis()->SetRangeUser(0.,500);
   hmt2bAll->GetHistogram()->GetXaxis()->SetTitle("M^{T} (GeV)");
   hmt2bAll->GetHistogram()->GetYaxis()->SetTitle("events/10 GeV");
@@ -183,17 +214,25 @@
   hmt2bAll->Draw("histo");
   hmt2bData->Draw("esame");
   leg->Draw("same");
+  latex.SetTextFont(42);
+  latex.SetTextSize(0.04);
+  latex.DrawLatex(20,90,"CMS 2010 Preliminary");
+  latex.SetTextSize(0.035);
+  latex.DrawLatex(20,42,"#sqrt{s} = 7 TeV");
+  latex.DrawLatex(300,0.5,"#intLdt = 15.041 pb^{-1}");
 
   int bin1  = hmt1bAll->GetHistogram()->FindBin(100.);
   int bin2  = hmt1bAll->GetHistogram()->GetNbinsX()+1;
   float n1b = hmt1b[8]->Integral(bin1,bin2)+hmt1b[7]->Integral(bin1,bin2)+
               hmt1b[6]->Integral(bin1,bin2)+hmt1b[5]->Integral(bin1,bin2)+ 
-              hmt1b[4]->Integral(bin1,bin2)+hmt1b[3]->Integral(bin1,bin2);
+              hmt1b[4]->Integral(bin1,bin2)+hmt1b[3]->Integral(bin1,bin2)+
+              hmt1b[9]->Integral(bin1,bin2);
   float n2b = hmt2b[8]->Integral(bin1,bin2)+hmt2b[7]->Integral(bin1,bin2)+
               hmt2b[6]->Integral(bin1,bin2)+hmt2b[5]->Integral(bin1,bin2)+ 
-              hmt2b[4]->Integral(bin1,bin2)+hmt2b[3]->Integral(bin1,bin2);
-//   float n1b = hmt1b[7]->Integral(bin1,bin2);
-//   float n2b = hmt2b[7]->Integral(bin1,bin2);
+              hmt2b[4]->Integral(bin1,bin2)+hmt2b[3]->Integral(bin1,bin2)+
+              hmt2b[9]->Integral(bin1,bin2);
+  //    float n1b = hmt1b[7]->Integral(bin1,bin2);
+  //   float n2b = hmt2b[7]->Integral(bin1,bin2);
 
   cout << "******* MC " << endl;
   cout << "Total number of events with 1 b-jet (MC) = " << n1b << std::endl;
@@ -245,7 +284,10 @@
   cout << "Number of events with 1 b-jet (DATA) = " << n1bData << std::endl;
   cout << "Number of events with 2 b-jet (DATA) = " << n2bData << std::endl;
 
-  //n2bData = 3;
+  
+  n2bData = hmt2bData->Integral(0,bin1)*hmt2b[7]->Integral(bin1,bin2)/hmt2b[7]->Integral(0,bin1);
+  cout << "estimated number of 2b in data above 100 GeV (PORCATA!) = " << n2bData << endl; 
+
   
   if (n1bData!=0 && n2bData!=0 )
     {
