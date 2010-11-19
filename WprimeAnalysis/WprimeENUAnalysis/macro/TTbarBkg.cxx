4da2c1ac406209c5a86cbb094425a6750e98d10d
malberti <>
2010-11-19 13:48:41 +0000

    fix bug in errors calculation

--- WprimeAnalysis/WprimeENUAnalysis/macro/TTbarBkg.cxx
+++ WprimeAnalysis/WprimeENUAnalysis/macro/TTbarBkg.cxx
@@ -1,14 +1,14 @@
 
-
 {
+
   gROOT->LoadMacro("setTDRStyle.C");
   setTDRStyle();
   //gROOT->SetStyle("Plain");
   gStyle->SetOptStat(0);
   gStyle->SetOptTitle(0);
 
-  float lumi = 15.0416; //all
-      
+  float lumi = 34.9; //all
+   
   int N = 10;
   
   int nRe  = 10;
@@ -54,13 +54,14 @@
   TH1F *hBdisc[10];
   TH1F *hNjets[10];
 
-  for (int i = 0; i< N; i++){
+  for (int i = 0; i< N; i++){    
+
     hmt[i]    = (TH1F*)f[i]->Get("hmt");
     hmt1b[i]  = (TH1F*)f[i]->Get("hmt1b");
     hmt2b[i]  = (TH1F*)f[i]->Get("hmt2b");
     hBdisc[i] = (TH1F*)f[i]->Get("hBdisc");
     hNjets[i] = (TH1F*)f[i]->Get("hNjets");
-
+    
     hmt[i]->Scale(lumi);
     hmt1b[i]->Scale(lumi);
     hmt2b[i]->Scale(lumi);
@@ -93,13 +94,14 @@
 
  
   TLegend *leg = new TLegend (0.6,0.6,0.89,0.89);
-  leg->SetFillColor(0);
-  leg->SetBorderSize(0);
+  leg->SetFillColor(kWhite);
+  //leg->SetBorderStyle(1);
+  //leg->SetBorderSize(2);
   //leg->AddEntry(hmt[0],"W', M =  800 GeV","F");
   //leg->AddEntry(hmt[1],"W', M = 1000 GeV","F");
   //leg->AddEntry(hmt[2],"W', M = 1500 GeV","F");
   leg->AddEntry(hmt[8],"W #rightarrow e #nu","F");
-  leg->AddEntry(hmt[4],"DY #rightarrow ll","F");
+  leg->AddEntry(hmt[4],"DY #rightarrow l^{+} l^{-}","F");
   leg->AddEntry(hmt[5],"Dibosons","F");
   leg->AddEntry(hmt[6],"W #rightarrow #tau #nu","F");
   leg->AddEntry(hmt[7],"t#bar{t}","F");
@@ -146,22 +148,22 @@
   TCanvas * cnjets = new TCanvas("cnjets","cnjets",500,500);
   cnjets->SetLogy();
   hNjetsAll->Draw("a histo");
-  hNjetsAll->SetMinimum(0.01);
-  hNjetsAll->SetMaximum(1000000);
+  hNjetsAll->SetMinimum(0.1);
+  hNjetsAll->SetMaximum(10000000);
   hNjetsAll->GetHistogram()->GetXaxis()->SetRangeUser(0.,6);
   hNjetsAll->GetHistogram()->GetXaxis()->SetTitle("number of jets");
   hNjetsAll->GetHistogram()->GetYaxis()->SetTitle("events");
   hNjetsAll->GetHistogram()->GetXaxis()->SetRangeUser(0,6);
   hNjetsAll->Draw("histo");
   hNjetsData->Draw("esame");
-  leg->Draw("same");
   TLatex latex;
   latex.SetTextFont(42);
   latex.SetTextSize(0.04);
-  latex.DrawLatex(0.5,500000,"CMS 2010 Preliminary");
+  latex.DrawLatex(0.5,4100000,"CMS 2010 Preliminary");
   latex.SetTextSize(0.035);
-  latex.DrawLatex(0.5,100000,"#sqrt{s} = 7 TeV");
-  latex.DrawLatex(0.5,50000,"#intLdt = 15.041 pb^{-1}");
+  latex.DrawLatex(0.5,1000000,"#sqrt{s} = 7 TeV");
+  latex.DrawLatex(0.5,150000,"#intLdt = 34.9 pb^{-1}");
+  leg->Draw("same");
 
   TCanvas * cbdisc = new TCanvas("cbdisc","cbdisc",500,500);
   cbdisc->SetLogy();
@@ -174,13 +176,13 @@
   hBdiscAll->GetHistogram()->GetXaxis()->SetRangeUser(0,6);
   hBdiscAll->Draw("histo");
   hBdiscData->Draw("esame");
-  leg->Draw("same");
   latex.SetTextFont(42);
   latex.SetTextSize(0.04);
   latex.DrawLatex(0.5,150,"CMS 2010 Preliminary");
   latex.SetTextSize(0.035);
   latex.DrawLatex(0.5,80,"#sqrt{s} = 7 TeV");
-  latex.DrawLatex(0.5,30,"#intLdt = 15.041 pb^{-1}");
+  latex.DrawLatex(0.5,30,"#intLdt = 34.9 pb^{-1}");
+  leg->Draw("same");
 
 
   TCanvas * c1b = new TCanvas("c1b","c1b",500,500);
@@ -194,13 +196,13 @@
   hmt1bAll->GetHistogram()->GetXaxis()->SetRangeUser(0,500);
   hmt1bAll->Draw("histo");
   hmt1bData->Draw("esame");
-  leg->Draw("same");
   latex.SetTextFont(42);
   latex.SetTextSize(0.04);
   latex.DrawLatex(20,90,"CMS 2010 Preliminary");
   latex.SetTextSize(0.035);
   latex.DrawLatex(20,42,"#sqrt{s} = 7 TeV");
-  latex.DrawLatex(300,0.5,"#intLdt = 15.041 pb^{-1}");
+  latex.DrawLatex(300,0.5,"#intLdt = 34.9 pb^{-1}");
+  leg->Draw("same");
 
   TCanvas * c2b = new TCanvas("c2b","c2b",500,500);
   c2b->SetLogy();
@@ -213,26 +215,36 @@
   hmt2bAll->GetHistogram()->GetXaxis()->SetRangeUser(0,500);
   hmt2bAll->Draw("histo");
   hmt2bData->Draw("esame");
-  leg->Draw("same");
   latex.SetTextFont(42);
   latex.SetTextSize(0.04);
   latex.DrawLatex(20,90,"CMS 2010 Preliminary");
   latex.SetTextSize(0.035);
   latex.DrawLatex(20,42,"#sqrt{s} = 7 TeV");
-  latex.DrawLatex(300,0.5,"#intLdt = 15.041 pb^{-1}");
+  latex.DrawLatex(300,0.5,"#intLdt = 34.9 pb^{-1}");
+  leg->Draw("same");
+
+  TH1F *hmt1bBkgSubtracted = (TH1F*)hmt1bData->Clone("hmt1bBkgSubtracted");
+  float s = 1;
+  hmt1bBkgSubtracted->Add( hmt1bBkgSubtracted,hmt1b[3],1.,-1.*s);
+  hmt1bBkgSubtracted->Add( hmt1bBkgSubtracted,hmt1b[4],1.,-1.*s);
+  hmt1bBkgSubtracted->Add( hmt1bBkgSubtracted,hmt1b[5],1.,-1.*s);
+  hmt1bBkgSubtracted->Add( hmt1bBkgSubtracted,hmt1b[6],1.,-1.*s);
+  hmt1bBkgSubtracted->Add( hmt1bBkgSubtracted,hmt1b[8],1.,-1.*s);
+  hmt1bBkgSubtracted->Add( hmt1bBkgSubtracted,hmt1b[9],1.,-1.*s);
+
 
   int bin1  = hmt1bAll->GetHistogram()->FindBin(100.);
   int bin2  = hmt1bAll->GetHistogram()->GetNbinsX()+1;
-  float n1b = hmt1b[8]->Integral(bin1,bin2)+hmt1b[7]->Integral(bin1,bin2)+
+  float n1b = hmt1b[7]->Integral(bin1,bin2)+(hmt1b[8]->Integral(bin1,bin2)+
               hmt1b[6]->Integral(bin1,bin2)+hmt1b[5]->Integral(bin1,bin2)+ 
               hmt1b[4]->Integral(bin1,bin2)+hmt1b[3]->Integral(bin1,bin2)+
-              hmt1b[9]->Integral(bin1,bin2);
-  float n2b = hmt2b[8]->Integral(bin1,bin2)+hmt2b[7]->Integral(bin1,bin2)+
+	      hmt1b[9]->Integral(bin1,bin2));
+  float n2b = hmt2b[7]->Integral(bin1,bin2)+(hmt2b[8]->Integral(bin1,bin2)+
               hmt2b[6]->Integral(bin1,bin2)+hmt2b[5]->Integral(bin1,bin2)+ 
               hmt2b[4]->Integral(bin1,bin2)+hmt2b[3]->Integral(bin1,bin2)+
-              hmt2b[9]->Integral(bin1,bin2);
-  //    float n1b = hmt1b[7]->Integral(bin1,bin2);
-  //   float n2b = hmt2b[7]->Integral(bin1,bin2);
+	      hmt2b[9]->Integral(bin1,bin2));
+  //float n1b = hmt1b[7]->Integral(bin1,bin2);
+  //float n2b = hmt2b[7]->Integral(bin1,bin2);
 
   cout << "******* MC " << endl;
   cout << "Total number of events with 1 b-jet (MC) = " << n1b << std::endl;
@@ -265,41 +277,76 @@
   float eA2 = 0.01;
 
   float eb = (A1/A2+2)/(n1b/n2b+2);
-  float eeb = (pow(eA1/A1,2) + pow(eA2/A2,2))/(pow(A1/A2+2,2)) + (1./n1b + 1./n2b)/(pow(n1b/n2b+2,2));
-  eeb = eb * sqrt(eeb);
+  float NN = (A1/A2+2);
+  float sigmaNN = A1/A2 * sqrt( pow(eA1/A1,2) + pow(eA2/A2,2) );
+  float DD = (n1b/n2b+2);
+  float sigmaDD = (n1b/n2b) * sqrt( 1./n1b + 1./n2b );
+  float eeb = eb * sqrt( pow(sigmaNN/NN,2) + pow(sigmaDD/DD,2) );
   cout<<"b-tagging efficiency = "<< eb <<" +/- "<<eeb<<endl;
 
+//    float nexp = n2b/(eb*eb*A2);
+//    float errn = nexp*sqrt(1./n2b + pow(eA2/A2,2) + pow(2*eeb/eb,2));
+//    cout<<"Estimated N(ttbar) from b-tag method = "<< nexp <<" +/- "<< errn <<endl;
+ 
+//    float nexp= n1b/(eb*A1+2*eb*A2*(1-eb));
+//    float D = eb*A1 + 2*eb*A2*(1-eb);
+//    float sigmaD = sqrt( pow(eb*eA1,2) + pow(A1*eeb,2) + pow(2*A2*eeb,2) + pow(2*eb*eA2,2) + pow(2*A2*2*eb*eeb,2) + pow(2*eb*eb*eA2,2));
+//    float errn = nexp*sqrt(1./n1b + pow(sigmaD/D,2));
+//    cout<<"Estimated N(ttbar) from b-tag method = "<< nexp <<" +/- "<< errn <<endl;
+
+
   float nexp = n2b/(eb*eb*A2);
-  float errn = nexp*sqrt(1./n2b + eA2/A2*eA2/A2 + pow(2*eeb/eb,2));
-  cout<<"True # of ttbar events = "<<hmt[7]->Integral(bin1,bin2)<<endl;
+  float K = (n1b+2*n2b)/(A1+2*A2);
+  float sigmaK = K*sqrt( (n1b+4*n2b)/(n1b+2*n2b)/(n1b+2*n2b) + (eA1*eA1+4*eA2*eA2)/(A1+2*A2)/(A1+2*A2) );
+  float errn   = nexp*sqrt(1./n2b + pow(eA2/A2,2) + pow(sigmaK/K,2)); 
   cout<<"Estimated N(ttbar) from b-tag method = "<< nexp <<" +/- "<< errn <<endl;
+
+
+  cout<<"True # of ttbar events = "<<hmt[7]->Integral(bin1,bin2)<<endl;
   cout<<endl;
  
 
 
   cout << "**** DATA " << endl;
-  float n1bData = hmt1bData->Integral(bin1,bin2);
+  //float n1bData = hmt1bData->Integral(bin1,bin2);
+  float n1bData = hmt1bBkgSubtracted->Integral(bin1,bin2);
   float n2bData = hmt2bData->Integral(bin1,bin2);
   
   cout << "Number of events with 1 b-jet (DATA) = " << n1bData << std::endl;
   cout << "Number of events with 2 b-jet (DATA) = " << n2bData << std::endl;
 
   
-  n2bData = hmt2bData->Integral(0,bin1)*hmt2b[7]->Integral(bin1,bin2)/hmt2b[7]->Integral(0,bin1);
-  cout << "estimated number of 2b in data above 100 GeV (PORCATA!) = " << n2bData << endl; 
+  //n2bData = hmt2bData->Integral(0,bin1)*hmt2b[7]->Integral(bin1,bin2)/hmt2b[7]->Integral(0,bin1);
+  //cout << "estimated number of 2b in data above 100 GeV (PORCATA!) = " << n2bData << endl; 
 
   
   if (n1bData!=0 && n2bData!=0 )
     {
-      eb = (A1/A2+2)/(n1bData/n2bData+2);
-      eeb = (pow(eA1/A1,2) + pow(eA2/A2,2))/(pow(A1/A2+2,2)) + (1./n1bData + 1./n2bData)/(pow(n1bData/n2bData+2,2));
-      eeb = eb * sqrt(eeb);
+      float eb = (A1/A2+2)/(n1bData/n2bData+2);
+      float NN = (A1/A2+2);
+      float sigmaNN = A1/A2 * sqrt( pow(eA1/A1,2) + pow(eA2/A2,2) );
+      float DD = (n1bData/n2bData+2);
+      float sigmaDD = ( n1bData/n2bData) * sqrt( 1./n1bData + 1./n2bData );
+      float eeb = eb * sqrt( pow(sigmaNN/NN,2) + pow(sigmaDD/DD,2) );
       cout<<"b-tagging efficiency = "<< eb <<" +/- "<<eeb<<endl;
       
       float nexpData = n2bData/(eb*eb*A2);
-      errn = nexpData*sqrt(1./n2bData + eA2/A2*eA2/A2 + pow(2*eeb/eb,2));
+      float errn = nexpData*sqrt(1./n2bData + eA2/A2*eA2/A2 + pow(2*eeb/eb,2));
       cout<<"MC # of ttbar events = "<<hmt[7]->Integral(bin1,bin2)<<endl;
       cout<<"Estimated N(ttbar) from b-tag method (DATA) = "<< nexpData <<" +/- "<< errn <<endl;
       cout<<endl;
     }
+
+  if (n2bData==0){
+    cout << "n2b = 0 in data ---> use b-tagging efficiency from MC :"<<endl;
+    eeb = 0.5*eb;
+    float nexpData = n1bData/(eb*A1+2*eb*A2*(1-eb));
+    float D = eb*A1 + 2*eb*A2*(1-eb);
+    float sigmaD = sqrt( pow(eb*eA1,2) + pow(A1*eeb,2) + pow(2*A2*eeb,2) + pow(2*eb*eA2,2) + pow(2*A2*2*eb*eeb,2) + pow(2*eb*eb*eA2,2));
+    float errn = nexp*sqrt(1./n1bData + pow(sigmaD/D,2));
+    cout<<"MC # of ttbar events = "<<hmt[7]->Integral(bin1,bin2)<<endl;
+    cout<<"Estimated N(ttbar) from b-tag method (DATA) = "<< nexpData <<" +/- "<< errn <<endl;
+    cout<<endl;
+  }
+
 }
