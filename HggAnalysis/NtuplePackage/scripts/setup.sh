2c53cc34cb697d4c9eef44687fd1a0a99ad927ca
deguio <>
2011-02-22 14:21:26 +0000

    buffi

--- HggAnalysis/NtuplePackage/scripts/setup.sh
+++ HggAnalysis/NtuplePackage/scripts/setup.sh
@@ -1,7 +1,9 @@
 export THISDIR=`pwd`
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISDIR}/lib
+if [ -n "${DYLD_LIBRARY_PATH}" ]; then
 export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${THISDIR}/lib
+fi
 export PATH=${PATH}:${THISDIR}/bin
 export PATH=${PATH}:${THISDIR}/scripts
 
