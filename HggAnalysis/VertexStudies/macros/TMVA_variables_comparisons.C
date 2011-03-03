4a3ae8d9450de45bae013d1eb3680a5d2b48505e
malberti <>
2011-03-03 10:51:50 +0000

    fix variables names

--- HggAnalysis/VertexStudies/macros/TMVA_variables_comparisons.C
+++ HggAnalysis/VertexStudies/macros/TMVA_variables_comparisons.C
@@ -1,30 +1,48 @@
 {
+  gROOT->SetStyle("Plain");
   TTree* ntu1 = (TTree*)_file0->Get("TMVA_vertexTree");
   TTree* ntu2 = (TTree*)_file1->Get("TMVA_vertexTree");
   
   TH1F tracksNum1("tracksNum1","tracksNum1",200,0.,200.);
   tracksNum1.SetLineColor(2);
+  tracksNum1.SetFillColor(2);
+  tracksNum1.SetFillStyle(3002);
+
   TH1F tracksNum2("tracksNum2","tracksNum2",200,0.,200.);
   tracksNum2.SetLineColor(4);
+  tracksNum2.SetFillColor(4);
+  tracksNum2.SetFillStyle(3002);
 
   TH1F deltaPhi1("deltaPhi1","deltaPhi1",150,0.,3.15);
   deltaPhi1.SetLineColor(2);
+  deltaPhi1.SetFillColor(2);
+  deltaPhi1.SetFillStyle(3002);
+
   TH1F deltaPhi2("deltaPhi2","deltaPhi2",150,0.,3.15);
   deltaPhi2.SetLineColor(4);
+  deltaPhi2.SetFillColor(4);
+  deltaPhi2.SetFillStyle(3002);
 
-  ntu1->Draw("tracksNumPt05 >> tracksNum1","isSig == 1 && sum2PhoPt > 10 && sum2PhoPt < 15 ","goff");
-  ntu2->Draw("tracksNumPt05 >> tracksNum2","isSig == 1 && sum2PhoPt > 10 && sum2PhoPt < 15","goff");
+  ntu1->Draw("nTracksPt05 >> tracksNum1","isSig == 1 && sum2PhoPt > 10 && sum2PhoPt < 15 ","goff");
+  ntu2->Draw("nTracksPt05 >> tracksNum2","isSig == 1 && sum2PhoPt > 10 && sum2PhoPt < 15","goff");
 
   ntu1->Draw("deltaPhi_HSumPt >> deltaPhi1","isSig == 1 && sum2PhoPt > 10 && sum2PhoPt < 15 ","goff");
   ntu2->Draw("deltaPhi_HSumPt >> deltaPhi2","isSig == 1 && sum2PhoPt > 10 && sum2PhoPt < 15","goff");
 
+  TLegend leg (0.6, 0.8,0.8,0.89);
+  leg->SetFillColor(0);
+  leg->AddEntry(&tracksNum1,"MC Hgg","F");
+  leg->AddEntry(&tracksNum2,"MC Zee","F");
+
   TCanvas c1;
   tracksNum2.DrawNormalized();
   tracksNum1.DrawNormalized("sames");
+  leg ->Draw("same");
 
   TCanvas c2;
   deltaPhi2.DrawNormalized();
   deltaPhi1.DrawNormalized("sames");
+  leg ->Draw("same");
 
 
 }
