c92e3f6b731c11b18d0522843fe52a59d6271d9f
deguio <>
2011-09-09 10:32:21 +0000

    missing

---
+++ WprimeAnalysis/WprimeAnalysis/interface/LinkDef.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_LINKDEF_H 
+#define FUNCTIONS_LINKDEF_H
+
+#ifdef __CINT__
+
+#pragma link off all classes;
+#pragma link off all function;
+
+#pragma link C++ function PURescaleFactor (const int&);
+
+#endif
+
+#endif
