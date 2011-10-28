11238fce870f0dcb313e8ce0dcf82e03a14ef66d
malberti <>
2011-10-28 15:33:10 +0000

    new macro to use ranking as baseline

--- HggAnalysis/VertexStudies/macros/PlotEff2file.C
+++ HggAnalysis/VertexStudies/macros/PlotEff2file.C
@@ -2,93 +2,69 @@
   gROOT->LoadMacro("~/setTDRStyle.C");
   setTDRStyle();
 
-  int saveScaleFactors = 1;
+  int saveScaleFactors = 0;
 
   TFile *f[2];
-  f[0] = TFile::Open("outputEff/test_Zmumu_DATA2011_160404-165542.root"); 
-  f[1] = TFile::Open("outputEff/test_ZmumuMC_Summer2011_PUweights_160404-165542.root");
- 
-//   string legtitle1 = "H#rightarrow#gamma#gamma <PU>=6 (sumpt2)";
-//   string legtitle1bdt = "H#rightarrow#gamma#gamma <PU>=6 (Ranking)";
-
-//   string legtitle2 = "H#rightarrow#gamma#gamma <PU>=10 (sumpt2)";
-//   string legtitle2bdt = "H#rightarrow#gamma#gamma <PU>=10 (Ranking)";
+  f[0] = TFile::Open("outputEff/test_Zmumu_Run2011B-PromptReco-v1_AOD_upto178677.root");  
+  f[1] = TFile::Open("outputEff/test_Zmumu_Run2011B-PromptReco-v1_AOD_upto178677.root");
   
   // legend
-  string legtitle1 = "DATA Z#rightarrow#mu#mu(sumpt2)";
-  string legtitle1bdt = "DATA Z#rightarrow#mu#mu(Ranking)";
-  string legtitle2 = "MC Z#rightarrow#mu#mu(sumpt2)";
-  string legtitle2bdt = "MC Z#rightarrow#mu#mu(Ranking)";
+  string legtitle1    = "DATA Z#rightarrow#mu#mu (BDT)";
+  string legtitle1bdt = "DATA Z#rightarrow#mu#mu (RANK)";
+  string legtitle2    = "MC Z#rightarrow#mu#mu (BDT)";
+  string legtitle2bdt = "MC Z#rightarrow#mu#mu (RANK)";
 
   // rebinning
-  int nRePt = 2;
-  // if variable size bins
-  double xbins[13] = {0.,5.,10.,15.,20.,25.,30.,35.,40.,50.,70.,110.,400.};
+  int nRePt = 1;
 
   // histograms
   TH1F* NvtxAll[2];
-  TH1F* NvtxSumpt2[2];
+  TH1F* NvtxBaseline[2];
   TH1F* NvtxBDT[2];
 
   TH1F* PtAll[2];
-  TH1F* PtSumpt2[2];
+  TH1F* PtBaseline[2];
   TH1F* PtBDT[2];
 
   TH1* PtAllRebinned[2];
-  TH1* PtSumpt2Rebinned[2];
+  TH1* PtBaselineRebinned[2];
   TH1* PtBDTRebinned[2];
 
-  TH1F* EtaAll[2];
-  TH1F* EtaSumpt2[2];
-  TH1F* EtaBDT[2];
-
   TGraphAsymmErrors *effVsNvtx_BDT[2];
-  TGraphAsymmErrors *effVsNvtx_sumpt2[2];
+  TGraphAsymmErrors *effVsNvtx_Baseline[2];
 
   TGraphAsymmErrors *effVsPt_BDT[2];
-  TGraphAsymmErrors *effVsPt_sumpt2[2];
+  TGraphAsymmErrors *effVsPt_Baseline[2];
 
   TGraphAsymmErrors *effVsEta_BDT[2];
-  TGraphAsymmErrors *effVsEta_sumpt2[2];
+  TGraphAsymmErrors *effVsEta_Baseline[2];
 
   int mycolor, mystyle;
   char hname[100];
 
   for (int i = 0; i < 2; i++){
     
-    NvtxAll[i] = (TH1F*) f[i]->Get("NvtAll");
-    NvtxSumpt2[i] = (TH1F*) f[i]->Get("NvtGood");
-    NvtxBDT[i] = (TH1F*) f[i]->Get("NvtGood_RANK");
+    NvtxAll[i]    = (TH1F*) f[i]->Get("NvtAll");
+    NvtxBaseline[i] = (TH1F*) f[i]->Get("NvtGood_RANK");
+    NvtxBDT[i]    = (TH1F*) f[i]->Get("NvtGood_BDT");
 
-    PtAll[i] = (TH1F*) f[i]->Get("PtAll");
-    PtSumpt2[i] = (TH1F*) f[i]->Get("PtGood");
-    PtBDT[i] = (TH1F*) f[i]->Get("PtGood_RANK");
+    PtAll[i]      = (TH1F*) f[i]->Get("PtAll");
+    PtBaseline[i]   = (TH1F*) f[i]->Get("PtGood_RANK");
+    PtBDT[i]      = (TH1F*) f[i]->Get("PtGood_BDT");
 
-    EtaAll[i] = (TH1F*) f[i]->Get("EtaAll");
-    EtaSumpt2[i] = (TH1F*) f[i]->Get("EtaGood");
-    EtaBDT[i] = (TH1F*) f[i]->Get("EtaGood_RANK");
-  
-    sprintf(hname,"PtAllRebinned_%d",i);
-    PtAllRebinned[i] = PtAll[i]   ->Rebin(12,hname,xbins);
-    sprintf(hname,"PtSumpt2Rebinned_%d",i);
-    PtSumpt2Rebinned[i] = PtSumpt2[i]->Rebin(12,hname,xbins);
-    sprintf(hname,"PtBDTRebinned_%d",i);
-    PtBDTRebinned[i] = PtBDT[i]   ->Rebin(12,"PtBDTRebinned",xbins);
+    PtAll[i]      -> Rebin(nRePt);
+    PtBaseline[i]   -> Rebin(nRePt);
+    PtBDT[i]      -> Rebin(nRePt);
 
-    effVsNvtx_BDT[i]     = new TGraphAsymmErrors();
-    effVsNvtx_sumpt2[i]  = new TGraphAsymmErrors();
+    effVsNvtx_BDT[i]   = new TGraphAsymmErrors();
+    effVsNvtx_Baseline[i]= new TGraphAsymmErrors();
     effVsPt_BDT[i]     = new TGraphAsymmErrors();
-    effVsPt_sumpt2[i]  = new TGraphAsymmErrors();
-    effVsEta_BDT[i]    = new TGraphAsymmErrors();
-    effVsEta_sumpt2[i] = new TGraphAsymmErrors();
-
-    if (i==0) {
-      mycolor = kGreen+1;
-    }
-    else {mycolor = kRed;
-    }
+    effVsPt_Baseline[i]  = new TGraphAsymmErrors();
+  
+    if (i==0) {mycolor = kGreen+1;}
+    else {mycolor = kRed;}
     
-      effVsNvtx_BDT[i]->SetLineColor(mycolor);
+    effVsNvtx_BDT[i]->SetLineColor(mycolor);
     effVsNvtx_BDT[i]->SetMarkerColor(mycolor);
     effVsNvtx_BDT[i]->SetMarkerStyle(24);
 
@@ -96,53 +72,36 @@
     effVsPt_BDT[i]->SetMarkerColor(mycolor);
     effVsPt_BDT[i]->SetMarkerStyle(24);
 
-    effVsEta_BDT[i]->SetLineColor(mycolor);
-    effVsEta_BDT[i]->SetMarkerColor(mycolor);
-    effVsEta_BDT[i]->SetMarkerStyle(24);
+    effVsNvtx_Baseline[i]->SetLineColor(mycolor);
+    effVsNvtx_Baseline[i]->SetMarkerColor(mycolor);
+    effVsNvtx_Baseline[i]->SetMarkerStyle(20);
 
-    effVsNvtx_sumpt2[i]->SetLineColor(mycolor);
-    effVsNvtx_sumpt2[i]->SetMarkerColor(mycolor);
-    effVsNvtx_sumpt2[i]->SetMarkerStyle(20);
-
-    effVsPt_sumpt2[i]->SetLineColor(mycolor);
-    effVsPt_sumpt2[i]->SetMarkerColor(mycolor);
-    effVsPt_sumpt2[i]->SetMarkerStyle(20);
-
-    effVsEta_sumpt2[i]->SetLineColor(mycolor);
-    effVsEta_sumpt2[i]->SetMarkerColor(mycolor);
-    effVsEta_sumpt2[i]->SetMarkerStyle(20);
+    effVsPt_Baseline[i]->SetLineColor(mycolor);
+    effVsPt_Baseline[i]->SetMarkerColor(mycolor);
+    effVsPt_Baseline[i]->SetMarkerStyle(20);
 
   }
 
   
   for (int i = 0; i<2; i++){
 
-    effVsNvtx_sumpt2[i]->BayesDivide(NvtxSumpt2[i],NvtxAll[i], "cp"); 
+    effVsNvtx_Baseline[i]->BayesDivide(NvtxBaseline[i],NvtxAll[i], "cp"); 
     effVsNvtx_BDT[i]   ->BayesDivide(NvtxBDT[i],NvtxAll[i], "cp");
- 
-   //  effVsPt_sumpt2[i]->BayesDivide(PtSumpt2[i],PtAll[i], "cp"); 
-//     effVsPt_BDT[i]   ->BayesDivide(PtBDT[i],PtAll[i], "cp"); 
-    effVsPt_sumpt2[i]->BayesDivide(PtSumpt2Rebinned[i],PtAllRebinned[i], "cp"); 
-    effVsPt_BDT[i]   ->BayesDivide(PtBDTRebinned[i],PtAllRebinned[i], "cp"); 
-
-    effVsEta_sumpt2[i]->BayesDivide(EtaSumpt2[i],EtaAll[i], "cp"); 
-    effVsEta_BDT[i]   ->BayesDivide(EtaBDT[i],EtaAll[i], "cp"); 
     
+    effVsPt_Baseline[i]->BayesDivide(PtBaseline[i],PtAll[i], "cp"); 
+    effVsPt_BDT[i]   ->BayesDivide(PtBDT[i],PtAll[i], "cp"); 
+        
   }
 
-
-  
-  int nMBMax =16;
+  int nMBMax = 30;
   
   TLegend legend1(0.68, 0.78, 0.99, 0.99);
   legend1.SetFillColor(kWhite);
   legend1.SetBorderSize(1);
-  legend1.AddEntry(effVsNvtx_sumpt2[0],legtitle1.c_str(),"LP");
-  legend1.AddEntry(effVsNvtx_sumpt2[1],legtitle2.c_str(),"LP");
+  legend1.AddEntry(effVsNvtx_Baseline[0],legtitle1.c_str(),"LP");
+  legend1.AddEntry(effVsNvtx_Baseline[1],legtitle2.c_str(),"LP");
   legend1.AddEntry(effVsNvtx_BDT[0],legtitle1bdt.c_str(),"LP");
   legend1.AddEntry(effVsNvtx_BDT[1],legtitle2bdt.c_str(),"LP");
-
-
 
   //*** EFF vs NVTX
   TCanvas c1;
@@ -150,12 +109,12 @@
   c1.SetGridy();
   TH2F cc("cc","",nMBMax+1,0,nMBMax+1,1000,0.,1.1);
   cc.SetStats(0); 
-  // cc.GetXaxis()->SetTitle("number of reconstructed vertices"); 
-  cc.GetXaxis()->SetTitle("number of simulated PU vertices"); 
-  cc.GetYaxis()->SetTitle("efficiency");
+  cc.GetXaxis()->SetTitle("number of reconstructed vertices"); 
+  //cc.GetXaxis()->SetTitle("number of simulated PU vertices"); 
+  cc.GetYaxis()->SetTitle("fraction of events");
   cc.Draw();
   for (int i = 0; i< 2; i++){
-    effVsNvtx_sumpt2[i]->Draw("p,same");
+    effVsNvtx_Baseline[i]->Draw("p,same");
     effVsNvtx_BDT[i]->Draw("p,same");
   }
   legend1.Draw("same");
@@ -165,27 +124,26 @@
   TCanvas c2;
   c2.SetGridx();
   c2.SetGridy();
-  TH2F dd("dd","",500,0,500,1000,0.0,1.1);
+  TH2F dd("dd","",200,0,200,1000,0.0,1.1);
   dd.SetStats(0); 
-  dd.GetXaxis()->SetTitle("boson p^{T} (GeV) "); 
-  dd.GetYaxis()->SetTitle("efficiency");
+  dd.GetXaxis()->SetTitle("p_{T}(Z) (GeV/c)"); 
+  dd.GetYaxis()->SetTitle("fraction of events");
   dd.Draw();
   for (int i = 0; i< 2; i++){
-    effVsPt_sumpt2[i]->Draw("p,same");
+    effVsPt_Baseline[i]->Draw("p,same");
     effVsPt_BDT[i]->Draw("p,same");
   }
   legend1.Draw("same");
-  
-  
+    
   float ptlow = 0.;
   float pthigh = 400.;
 
-  int bin1 = PtSumpt2[0]->FindBin(ptlow);
-  int bin2 = PtSumpt2[0]->FindBin(pthigh);
+  int bin1 = PtBaseline[0]->FindBin(ptlow);
+  int bin2 = PtBaseline[0]->FindBin(pthigh);
 
    
-  double eff1 = PtSumpt2[0]->Integral(bin1,bin2)/ PtAll[0]->Integral(bin1,bin2);
-  double eff2 = PtSumpt2[1]->Integral(bin1,bin2)/ PtAll[1]->Integral(bin1,bin2);
+  double eff1 = PtBaseline[0]->Integral(bin1,bin2)/ PtAll[0]->Integral(bin1,bin2);
+  double eff2 = PtBaseline[1]->Integral(bin1,bin2)/ PtAll[1]->Integral(bin1,bin2);
   
   double eff1bdt = PtBDT[0]->Integral(bin1,bin2)/ PtAll[0]->Integral(bin1,bin2);
   double eff2bdt = PtBDT[1]->Integral(bin1,bin2)/ PtAll[1]->Integral(bin1,bin2);
@@ -200,22 +158,22 @@
   cout << legtitle2bdt.c_str() << " --> eff = " << eff2bdt << " +/- " 
        << sqrt(eff2bdt*(1-eff2bdt)/PtAll[1]->Integral(bin1,bin2))<<endl;  
   
-  //RATIOs
+  //EFFICIENCY RATIOs
 
   //------ RATIO EFF vs PT -------------------------------------------
-  TGraphErrors * ratioEffVsPt_sumpt2 = new TGraphErrors();
-  ratioEffVsPt_sumpt2 ->SetMarkerStyle(20);
+  TGraphErrors * ratioEffVsPt_Baseline = new TGraphErrors();
+  ratioEffVsPt_Baseline ->SetMarkerStyle(20);
   
-  int nbins = effVsPt_sumpt2[0]->GetHistogram()->GetNbinsX();
+  int nbins = effVsPt_Baseline[0]->GetHistogram()->GetNbinsX();
   
   double pt, err1, err2, ratio , err;
   
   for (int i = 0; i < nbins; i++){
-    effVsPt_sumpt2[0]->GetPoint(i,pt,eff1);
-    effVsPt_sumpt2[1]->GetPoint(i,pt,eff2);
+    effVsPt_Baseline[0]->GetPoint(i,pt,eff1);
+    effVsPt_Baseline[1]->GetPoint(i,pt,eff2);
     
-    err1 =  effVsPt_sumpt2[0]->GetErrorY(i);
-    err2 =  effVsPt_sumpt2[1]->GetErrorY(i);
+    err1 =  effVsPt_Baseline[0]->GetErrorY(i);
+    err2 =  effVsPt_Baseline[1]->GetErrorY(i);
     
     if (eff2!=0 && eff1!=0)   {
       ratio = eff1/eff2;
@@ -225,13 +183,13 @@
       err = 0;
     }
     
-    ratioEffVsPt_sumpt2->SetPoint(i,pt,ratio);
-    ratioEffVsPt_sumpt2->SetPointError(i, effVsPt_sumpt2[0]->GetErrorX(i) ,err);
+    ratioEffVsPt_Baseline->SetPoint(i,pt,ratio);
+    ratioEffVsPt_Baseline->SetPointError(i, effVsPt_Baseline[0]->GetErrorX(i) ,err);
   }
   
   TGraphErrors * ratioEffVsPt_BDT = new TGraphErrors();
   ratioEffVsPt_BDT ->SetMarkerStyle(24);
-
+  
   for (int i = 0; i < nbins; i++){
     effVsPt_BDT[0]->GetPoint(i,pt,eff1);
     effVsPt_BDT[1]->GetPoint(i,pt,eff2);
@@ -257,36 +215,36 @@
   cRatioPt.SetGridx();
   cRatioPt.SetGridy();
 
-  ratioEffVsPt_sumpt2->GetHistogram()->GetXaxis()->SetTitle("boson p^{T} (GeV)");
-  ratioEffVsPt_sumpt2->GetHistogram()->GetYaxis()->SetTitle("eff(data)/eff(MC)");
-  ratioEffVsPt_sumpt2->GetHistogram()->GetXaxis()->SetRangeUser(0,200);
-  ratioEffVsPt_sumpt2->GetHistogram()->GetYaxis()->SetRangeUser(0.7,1.3);
-  ratioEffVsPt_sumpt2->Draw("ap");
+  ratioEffVsPt_Baseline->GetHistogram()->GetXaxis()->SetTitle("p_{T}(Z) (GeV/c)");
+  ratioEffVsPt_Baseline->GetHistogram()->GetYaxis()->SetTitle("#epsilon(data)/#epsilon(MC)");
+  ratioEffVsPt_Baseline->GetHistogram()->GetXaxis()->SetRangeUser(0,200);
+  ratioEffVsPt_Baseline->GetHistogram()->GetYaxis()->SetRangeUser(0.7,1.3);
+  ratioEffVsPt_Baseline->Draw("ap");
   ratioEffVsPt_BDT->Draw("psame");
 
   TLegend legend4(0.15, 0.2, 0.45, 0.4);
   legend4.SetFillColor(kWhite);
   legend4.SetBorderSize(1);
-  legend4.AddEntry(ratioEffVsPt_sumpt2,"sumpt2","LP");
-  legend4.AddEntry(ratioEffVsPt_BDT,"ranking","LP");
+  legend4.AddEntry(ratioEffVsPt_Baseline,"RANK","LP");
+  legend4.AddEntry(ratioEffVsPt_BDT,"BDT","LP");
   legend4.Draw("same");
 
 
 
   //------ RATIO EFF vs NVTX -------------------------------------------
-  TGraphErrors * ratioEffVsNvtx_sumpt2 = new TGraphErrors();
-  ratioEffVsNvtx_sumpt2 ->SetMarkerStyle(20);
+  TGraphErrors * ratioEffVsNvtx_Baseline = new TGraphErrors();
+  ratioEffVsNvtx_Baseline ->SetMarkerStyle(20);
   
-  int nbins = effVsNvtx_sumpt2[0]->GetHistogram()->GetNbinsX();
+  int nbins = effVsNvtx_Baseline[0]->GetHistogram()->GetNbinsX();
   
   double pt, err1, err2, ratio , err;
   
   for (int i = 0; i < nbins; i++){
-    effVsNvtx_sumpt2[0]->GetPoint(i,pt,eff1);
-    effVsNvtx_sumpt2[1]->GetPoint(i,pt,eff2);
+    effVsNvtx_Baseline[0]->GetPoint(i,pt,eff1);
+    effVsNvtx_Baseline[1]->GetPoint(i,pt,eff2);
     
-    err1 =  effVsNvtx_sumpt2[0]->GetErrorY(i);
-    err2 =  effVsNvtx_sumpt2[1]->GetErrorY(i);
+    err1 =  effVsNvtx_Baseline[0]->GetErrorY(i);
+    err2 =  effVsNvtx_Baseline[1]->GetErrorY(i);
     
     if (eff2!=0 && eff1!=0)   {
       ratio = eff1/eff2;
@@ -296,8 +254,8 @@
       err = 0;
     }
     
-    ratioEffVsNvtx_sumpt2->SetPoint(i,pt,ratio);
-    ratioEffVsNvtx_sumpt2->SetPointError(i,effVsNvtx_sumpt2[0]->GetErrorX(i),err);
+    ratioEffVsNvtx_Baseline->SetPoint(i,pt,ratio);
+    ratioEffVsNvtx_Baseline->SetPointError(i,effVsNvtx_Baseline[0]->GetErrorX(i),err);
   }
   
   TGraphErrors * ratioEffVsNvtx_BDT = new TGraphErrors();
@@ -326,24 +284,27 @@
   cRatioNvtx.SetGridx();
   cRatioNvtx.SetGridy();
 
-  ratioEffVsNvtx_sumpt2->GetHistogram()->GetXaxis()->SetTitle("number of reconstructed vertices");
-  ratioEffVsNvtx_sumpt2->GetHistogram()->GetYaxis()->SetTitle("eff(data)/eff(MC)");
-  ratioEffVsNvtx_sumpt2->GetHistogram()->GetXaxis()->SetRangeUser(0,16);
-  ratioEffVsNvtx_sumpt2->GetHistogram()->GetYaxis()->SetRangeUser(0.7,1.3);
-  ratioEffVsNvtx_sumpt2->Draw("ap");
+  ratioEffVsNvtx_Baseline->GetHistogram()->GetXaxis()->SetTitle("number of reconstructed vertices");
+  ratioEffVsNvtx_Baseline->GetHistogram()->GetYaxis()->SetTitle("#epsilon(data)/#epsilon(MC)");
+  ratioEffVsNvtx_Baseline->GetHistogram()->GetXaxis()->SetRangeUser(0,16);
+  ratioEffVsNvtx_Baseline->GetHistogram()->GetYaxis()->SetRangeUser(0.7,1.3);
+  ratioEffVsNvtx_Baseline->Draw("ap");
   ratioEffVsNvtx_BDT->Draw("psame");
 
 
   TLegend legend5(0.15, 0.2, 0.45, 0.4);
   legend5.SetFillColor(kWhite);
   legend5.SetBorderSize(1);
-  legend5.AddEntry(ratioEffVsNvtx_sumpt2,"sumpt2","LP");
-  legend5.AddEntry(ratioEffVsNvtx_BDT,"ranking","LP");
+  legend5.AddEntry(ratioEffVsNvtx_Baseline,"RANK","LP");
+  legend5.AddEntry(ratioEffVsNvtx_BDT,"BDT","LP");
 
   legend5.Draw("same");
 
   if (saveScaleFactors){
-    TFile *fileout = new TFile("vtxIdScaleFactorFromZmumu_Summer11_test.root","recreate");
+    //    TFile *fileout = new TFile("vtxIdScaleFactorFromZmumu_Summer11_Puweights160404-167151.root","recreate");
+    //TFile *fileout = new TFile("vtxIdScaleFactorFromZmumu_Summer11_Puweights_lp.root","recreate");
+    //TFile *fileout = new TFile("vtxIdScaleFactorFromZmumu_Summer11_Puweights_eps.root","recreate");
+    TFile *fileout = new TFile("vtxIdScaleFactorFromZmumu_Summer11_Puweights_after_eps.root","recreate");
     ratioEffVsPt_BDT->SetTitle("scaleFactor");
     ratioEffVsPt_BDT->Write("scaleFactor");
   }
