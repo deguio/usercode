def322234a93a028e119598b505e1ba1ac53b06b
malberti <>
2012-05-07 13:49:57 +0000

    macro to compute weights

--- HggAnalysis/VertexStudies/weights/ComputeWeights.C
+++ HggAnalysis/VertexStudies/weights/ComputeWeights.C
@@ -1,29 +1,18 @@
 //****** simple macro to compute PU weights ******
 {
   //*** mc file
-  TFile *fmc = TFile::Open("HistosForPUReweighting_DYJetsToLL_Fall11_S6.root");
-  TH1F *hmc  = (TH1F*)fmc->Get("hnpumc");
+  TFile *fmc = TFile::Open("../weights/HistosForPUReweighting_DYJetsToLL_Summer12_S9.root");
+  TH1F *hmc  = (TH1F*)fmc->Get("htruenpumc");
  
   //*** data file 
-  //TFile *fda = TFile::Open("/afs/cern.ch/user/a/adavidzh/public/json/111105/pileup/2011A_0100_73500.pileup.root");
-  TFile *fda = TFile::Open("/afs/cern.ch/user/a/adavidzh/public/json/111105/pileup/2011B_0100_73500.pileup.root");
-  //  TFile *fda = TFile::Open("/afs/cern.ch/user/a/adavidzh/public/json/111105/pileup/2011_0100_73500.pileup.root");
-
-  TH1F *pileup = (TH1F*)fda->Get("pileup");
-
-  TH1F *hdata = new TH1F("hdata","hdata",60,0,60);
-  for (int ibin = 1; ibin < 61; ibin++){
-    hdata->SetBinContent(ibin, pileup->GetBinContent(ibin));
-  }
+  TFile *fda = TFile::Open("../pileup/pileup_190389-191859_minBiasXsec62460.root");
+  TH1F *hdata = (TH1F*)fda->Get("pileup");
  
-
   //*** compute weights
   TH1F *hweights = (TH1F*)hdata->Clone("hweights");
   hweights->Divide(hdata,hmc,1./hdata->GetSumOfWeights(),1./hmc->GetSumOfWeights());
 
-  //TFile *fout = new TFile("./PUweights_2011A_0100_73500_DYJetsToLL_Fall11_S6.root","create");
-  TFile *fout = new TFile("./PUweights_2011B_0100_73500_DYJetsToLL_Fall11_S6.root","create");
-  //TFile *fout = new TFile("./PUweights_2011_0100_73500_DYJetsToLL_Fall11_S6.root","create");
+  TFile *fout = new TFile("./PUweights_2012_DYJetsToLL_Summer12_S9_minBiasXsec62460.root","create");
   hweights->Write("hweights");
 
 
